8c5b418e3182b29aa7970538ef8e42c4c51fb3cd
SuperNova_74_K <kingkhaled.w@gmail.com>
2022-04-05 15:55:13 +0200

    Added file save function with defensive.

--- Main_menu.cpp
+++ Main_menu.cpp
@@ -14,6 +14,7 @@ void print_options();
 void excute_choice(string choice);
 string get_choice(string allowed);
 void load_img(int pic_number);
+int find(vector<string>vec, string target);
 
 unsigned char img[SIZE][SIZE];
 unsigned char img2[SIZE][SIZE];
@@ -26,12 +27,18 @@ int main(){
     string choice = "random";
     while(choice != "0"){
         print_options();
-        choice = get_choice("1234567890abcs");
+        vector<string>allowed = {"1","2","3","4","5","6","7","8","9","0","a","b","c","s"};
+        // could have done this with regex but I wanted Custom function for easier debugging
+        choice = get_choice(allowed);
         excute_choice(choice);
         clear();
         if(choice != "0"){
             cout << "Filter Applied.";
             sleep(1.5);
+        }else{
+            clear();
+            cout << "Thanks for using our program.";
+            return 0;
         }
     }
     
@@ -60,18 +67,27 @@ void print_options(){
     cout << "(0) Exit"<< endl;
 }
 
-string get_choice(string allowed){
+int find(vector<string>vec, string target){
+    for(int i = 0; i < vec.size() ; i++){
+        if(vec[i] == target){
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+string get_choice(vector<string>allowed)){
+    string output;
     while (1)
     {
         cout << "Please enter option: ";
         string inpt; 
         getline(cin, inpt);
-        while((getchar()) != '\n');
-        string allowed;
-        int found = allowed.find(inpt);
+        // while((getchar()) != '\n');
+        int found = find(allowed,inpt);
         if(found != -1){
-            string output;
-            output += allowed[found];
+            output = allowed[found];
             return output;
         }else{
             clear();
@@ -99,36 +115,51 @@ void excute_choice(string choice){
     }
     else if(choice == "4"){
         cout << "Choose Mode:\n(1) Horizontal\n(2) Vertical" << endl;
-        string option = get_choice("12");
+        vector<string>allowed = {"1","2"};
+        string option = get_choice(allowed);
         bool style = (option == "1") ? true : false;
         flip(img, style);
     }
     else if(choice == "5"){
         cout << "Choose Mode:\n(1) Lighten\n(2) Darken" << endl;
-        string option = get_choice("12");
+        vector<string>allowed = {"1","2"};
+        string option = get_choice(allowed);
         bool style = (option == "1") ? true : false;
-        black_white(img);
+        darken_n_lighten(img,style);
     }
-    else if(choice == "6"){
-        black_white(img);
-    }
+    // else if(choice == "6"){
+    //     rotate
+    // }
     else if(choice == "7"){
-        black_white(img);
+        edge_detect(img);
     }
-    else if(choice == "8"){
-        black_white(img);
-    }
+    // else if(choice == "8"){
+    //     enlarge
+    // }
     else if(choice == "9"){
-        black_white(img);
+        cout << "Choose Mode:\n(1) 1/2\n(2) 1/3\n(3) 1/4" << endl;
+        vector<string>allowed = {"1","2","3"};
+        string option = get_choice(allowed);
+        shrink_img(img, option + 1);
     }
     else if(choice == "a"){
-        black_white(img);
+        cout << "Choose Mode:\n(1) 1/2\n(2) 1/3\n(3) 1/4" << endl;
+        vector<string>allowed = {"1","2","3","4"};
+        string option = get_choice(allowed);
+        mirror(img, allowed);
     }
-    else if(choice == "b"){
-        black_white(img);
-    }
+    // else if(choice == "b"){
+    //     Shuffle
+    // }
     else if(choice == "c"){
-        black_white(img);
+        blur_img(img);
+    }
+    else if(choice == "s"){
+        clear();
+        cout << "Please write the Target file name\nNo special characters allowed\n(IF THE FILE EXISTS, IT WILL BE OVERWRITTEN !): ";
+        string option;
+        getline(cin, option);
+
     }
     
 }
