081abd8c5790e5b43e2e5c97dfe8d927dd157049
SuperNova_74_K <kingkhaled.w@gmail.com>
2022-04-07 14:51:58 +0200

    Added comments & Header

--- Main_menu.cpp
+++ Main_menu.cpp
@@ -1,3 +1,26 @@
+/*
+    CS112 : Structured Programming Assignment 3 - First Submission
+    
+    Authors:
+    Khaled Waleed Salah , ID: 20210127
+    (hanafy write here your name and id as well as youssef's)
+
+
+    Prepared for : Dr.Mohammed El-Ramly , TA: Hager (write her second name hanafy)
+    Purpose : Photo Editor with multiple filters
+    Date : 7 April 2022
+
+    Version : 1.2
+    Scheduled Features : - Filter 6 & Filter B
+                         - Undo option
+                         - Display photo
+                         - Colorful theme for the Terminal / CMD
+                         - Fixing Clear() Function to work with Mac & Linux
+*/
+
+
+
+
 #include <iostream>
 #include <unistd.h> // to make sleep() work
 #include <fstream>
@@ -10,28 +33,43 @@
 
 using namespace std;
 
-void clear();
-void print_options();
-void excute_choice(string choice);
-string get_choice(vector<string>allowed, int mode, string message);
-void load_img(int pic_number);
-int find(vector<string>vec, string target);
+// funcitons prototypes
+void clear(); // to clear terminal for an elegant application
+void print_options(); // printing the main menu options
+void excute_choice(string choice); // to excute user choice
 
-unsigned char img[SIZE][SIZE];
-unsigned char img2[SIZE][SIZE];
+// this is the most important funciton, takes a vecotr of all allowed strings and a mode to determine what message to print
+// and finally a message for the user
+string get_choice(vector<string>allowed, int mode, string message);
+
+
+void load_img(int pic_number);
+int find(vector<string>vec, string target); // searches about a string in a vector of strings
+
+unsigned char img[SIZE][SIZE]; // declaring global img
+unsigned char img2[SIZE][SIZE]; // declaring second global img for some filters to work
+
+
+
+
 
 int main(){
-    cout << "Hello To Our Photoshop!";
+    cout << "Hello To Our Photo Editor!";
     sleep(1.5);
     clear();
     load_img(1);
-    string choice = "random";
+    string choice = "random"; // random intialization
+
+    // keep asking untill the user wants to exit
     while(choice != "0"){
-        vector<string>allowed = {"1","2","3","4","5","7","8","9","0","a","c","s","l"};
-        // could have done this with regex but I wanted Custom function for easier debugging
-        choice = get_choice(allowed,0, "d");
+        vector<string>allowed = {"1","2","3","4","5","7","8","9","0","a","c","s","l"}; // this is a vector of all acceptable inputs
+        // could have done this with regex but we wanted Custom function for easier debugging
+        
+        choice = get_choice(allowed,0, "d"); // getting the user choice using the fuction 
         excute_choice(choice);
         clear();
+
+        // to tell user about the success of the operation
         if(choice != "0" && choice != "s" && choice != "l"){
             cout << "Filter Applied."<< endl;
             sleep(1.5);
@@ -43,6 +81,7 @@ int main(){
             cout << "Image Loaded."<< endl;
             sleep(1.5);
         }
+        // if it survived all of this that means it's difinelty zero, so we exit the program
         else{
             clear();
             cout << "Thanks for using our program!";
@@ -79,11 +118,14 @@ void print_options(){
 }
 
 int find(vector<string>vec, string target){
+    // looping over the vecotr to search for the target
     for(int i = 0; i < vec.size() ; i++){
         if(vec[i] == target){
+            // if target found, return it's index
             return i;
         }
     }
+    // if it survived till here then return -1 to indicate it's not found
     return -1;
 }
 
@@ -92,6 +134,7 @@ string get_choice(vector<string>allowed, int mode, string message){
     string output;
     while (1)
     {
+        // if mode is zero then print the main menu, else print the message provided
         if(mode == 0){
             print_options();
         }else{
@@ -101,15 +144,18 @@ string get_choice(vector<string>allowed, int mode, string message){
         getline(cin, inpt);
         int found = find(allowed,inpt);
         if(found != -1){
+            // if the input was found in the allowed input then retun that input
             output = allowed[found];
             return output;
         }else{
+            // if it's not the case then give the user a message
             clear();
             cout << "wrong input, please enter a valid Option." << endl;
         }
     }
 
 }
+
 
 void excute_choice(string choice){
     clear();
@@ -124,10 +170,12 @@ void excute_choice(string choice){
         merge_filter(img, img2);
     }
     else if(choice == "4"){
+        // in this example we give allowed inputs "1" and "2" to the get_choice function as well as a message to input
+        // for the user and that funcitons does the defense for us and return only a good value.
         vector<string>allowed{"1","2"};
         string option = get_choice(allowed,1,"Choose Mode:\n(1) Horizontal\n(2) Vertical\nChoose an option: ");
         bool style = (option == "1");
-        flip(img, style);
+        flip(img, style); // feeding the filter the info needed to work
     }
     else if(choice == "5"){
         vector<string>allowed{"1","2"};
@@ -135,6 +183,7 @@ void excute_choice(string choice){
         bool style = (option == "1");
         darken_n_lighten(img,style);
     }
+    // we haven't done the filter 6 yet (will be done after the mid-terms)
     // else if(choice == "6"){
     //     rotate
     // }
@@ -156,24 +205,30 @@ void excute_choice(string choice){
         string option = get_choice(allowed,1,"Choose the part to mirror:\n(1) lower\n(2) upper\n(3) right\n(4) Left\nChoose an option: ");
         mirror(img, option);
     }
-    // else if(choice == "b"){
+    // else if(choice == "b"){ // we haven't done this filter yet, scheduled after the mid-terms
     //     Shuffle
     // }
     else if(choice == "c"){
         blur_img(img);
     }
+    // to save the image
     else if(choice == "s"){
-        strt_of_Zloop:
+        strt_of_Zloop: // a flag to be able to jumb here when needed
         while(true){
             clear();
             cout << "Please write the Target file name\n{\\/:*?\"<>|} characters are not allowed: ";
             string option = "";
             getline(cin, option);
 
+            // some special characters are not allowed in naming of files in windows so we prevent the user form messing the file
             vector<string> not_allowed{"\\", "/", ":", "*", "?", "\"", "<", ">", "|"};
             for(auto i : option){
+                // here we convert the char into a string to be able to pass it in the find() without function overloading
                 string tmp_to_chnge_chr_2_strng;
                 tmp_to_chnge_chr_2_strng += i;
+
+                // if any character in the input is not allowed, reject the output and the filename cannot be more than 254 character
+                // to prevent windows rejecting the file
                 if((find(not_allowed, tmp_to_chnge_chr_2_strng) != -1) || option.size() > 254){
                     clear();
                     cout << "This filename is not allowed !";
@@ -186,19 +241,27 @@ void excute_choice(string choice){
                 }
             }
 
-
+            // if filename was valid, we add the extention name
             option += ".bmp";
+
+            // this part is to check if the file exists and asking if the user is sure about overwriting it
             ifstream exist;
             exist.open(option);
+
+            // the function writeGSBMP that came with the library only supports the filename as C-String
+            // while we get the input form user as String Class for easier I/O so we make a copy of the string
+            // and turn that copy to C-string usin dynamic memory allocation
             char *option_as_C_string = new char [option.length()+1];
             strcpy(option_as_C_string, option.c_str());
+
+
             if(exist){
                 cout << "This file already exists, ARE YOU SURE you want to OVERWRITE it ? type (YES) for yes, anything else for no :";
                 string overwrite;
                 getline(cin, overwrite);
                 if(overwrite == "YES"){
                     writeGSBMP(option_as_C_string, img);
-                    delete[] option_as_C_string;
+                    delete[] option_as_C_string; // deleting the C-string to avoid memory leak
                     break;
                 }
                 else {
@@ -208,6 +271,7 @@ void excute_choice(string choice){
                     goto strt_of_Zloop;
                 }
             }else{
+                // if everything goes well we save the file
                 writeGSBMP(option_as_C_string, img);
                 delete[] option_as_C_string;
                 break;
@@ -223,41 +287,24 @@ void excute_choice(string choice){
 
 void load_img(int pic_number){
     if(pic_number == 1){
-        char inpt[100];
+        // the maximum filename size on windows is 255 character, so the user won't find a picture with a name bigger than that anyways
+        char inpt[255];
         do{
         cout << "Please enter the name of the photo you would like to edit: ";
-        cin.getline(inpt,100);
+        cin.getline(inpt,255);
         strcat(inpt,".bmp");
         clear();
         }
+        // we keep trying to get input untill the readGSBMP funciton returns anything else than 1 (which means file exists and valid)
         while(readGSBMP(inpt, img) == 1);
     }else{
-        char inpt[100];
+        char inpt[255];
         do{
         cout << "Please enter the name of the second photo: ";
-        cin.getline(inpt,100);
+        cin.getline(inpt,255);
         strcat(inpt,".bmp");
         clear();
         }
         while(readGSBMP(inpt, img2) == 1);
     }
 }
-
-// needed functions:
-/*
-clear function
-printing options
-function for printing message and getting input, while also validating it
-function for excuting the filter
-function for changing the loaded image
-funcition for loading image
-
-*/
-
-/*
-Program goes like this:
-
-print message for the user
-take and validate input
-excute
-*/
