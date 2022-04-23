ba663fa29d6e7e1073c7d92ad12d21e2d155489e
youssefAnwar87 <102333209+youssefAnwar87@users.noreply.github.com>
2022-04-23 20:19:25 +0200

    Update filter_group_2.h

--- filter_group_2.h
+++ filter_group_2.h
@@ -26,7 +26,7 @@ void shuffle_img(unsigned char img[][SIZE][RGB])
     unsigned char img2[SIZE][SIZE][RGB];
     retake_input:
 
-    int order[4];
+    int order[4];  //array to store input
     int num1{0}, num2{0};
     
     regex regex_filter("[1-4]{4}");
@@ -34,13 +34,13 @@ void shuffle_img(unsigned char img[][SIZE][RGB])
     do{    
         cout << "Enter the quarter's order : ";
         getline(cin, order_inpt);
-        for(int i = 0; i < order_inpt.length() - 1; i++){
+        for(int i = 0; i < order_inpt.length() - 1; i++){    //remove spaces between numbers
             if(order_inpt[i] == ' '){
                 order_inpt.erase(i,1);
                 i--;
             }
         }
-        if(!regex_match(order_inpt ,regex_filter)){
+        if(!regex_match(order_inpt ,regex_filter)){     //checks if input is valid or not ( from 1 to 4 )
             cout << endl <<"Invalid 546 input !" << endl;
             sleep(1);
             clear_terminal();
@@ -59,7 +59,7 @@ void shuffle_img(unsigned char img[][SIZE][RGB])
     {
         loopAgain = false;
 
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < 4; i++)    // nested loop to check if any number os repeated or not
         {
             for (int j = 0; j < 4; j++)
             {
@@ -78,15 +78,15 @@ void shuffle_img(unsigned char img[][SIZE][RGB])
             goto retake_input;
         }
     }
-    
+                              // num1 and num2 value change according to the chosen quarter
     
     for (int i = 0; i < 129; i++)
     {
         for (int j = 0; j < 129; j++)
         {
-            for (int k = 0; k < RGB; k++) {
+            for (int k = 0; k < RGB; k++) {               
 
-                if (order[0] == 1)
+                if (order[0] == 1)                   // Four IF Conditions check which quarter  is chosen to be  top left in the New Image
                 {
                     num1 = num2 = 0;
                 }
@@ -107,8 +107,7 @@ void shuffle_img(unsigned char img[][SIZE][RGB])
 
                 img2[i][j][k] = img[i + num1][j + num2][k];
                 
-                if (order[2] == 1)
-                {
+                if (order[2] == 1)      // Four IF Conditions check which quarter is chosen to be on Down left in the New Image  
                     num1 = num2 = 0;
                 }
                 else if (order[2] == 2)
@@ -128,7 +127,7 @@ void shuffle_img(unsigned char img[][SIZE][RGB])
 
                 img2[i + 127][j][k] = img[i + num1][j + num2][k];
 
-                if (order[1] == 1)
+                if (order[1] == 1)   // Four IF Conditions check which quater is chosen to be on the top Right in the New image
                 {
                     num1 = num2 = 0;
                 }
@@ -149,7 +148,7 @@ void shuffle_img(unsigned char img[][SIZE][RGB])
 
                 img2[i][j + 127][k] = img[i + num1][j + num2][k];
 
-                if (order[3] == 1)
+                if (order[3] == 1)    // Four IF Conditions check which quarter is chosen to be down Right in the New image
                 {
                     num1 = num2 = 0;
                 }
@@ -176,7 +175,7 @@ void shuffle_img(unsigned char img[][SIZE][RGB])
 
 
 
-    for (int i = 0; i < SIZE; i++)
+    for (int i = 0; i < SIZE; i++)   //copies which is done by the filter to image1 
     {
         for (int j = 0; j < SIZE; j++)
         {
@@ -196,7 +195,7 @@ void enlarge(unsigned char img[][SIZE][RGB] ,int quarter_num)
     int num1 = 0 ;
     int num2 = 0 ;
 
-    if (quarter_num == 1){
+    if (quarter_num == 1){   // num1 and num2 value change according to the chosen quarter
         num1 = num2 = 0;
     }
     else if (quarter_num ==2){
@@ -214,7 +213,7 @@ void enlarge(unsigned char img[][SIZE][RGB] ,int quarter_num)
         for (int j = 0; j< SIZE; j++) {
             for (int k = 0; k< RGB; k++)
             {
-              img2 [i][j][k] = img[ i/2 + num1][j/2 + num2][k];
+              img2 [i][j][k] = img[ i/2 + num1][j/2 + num2][k]; //Equation to perform Enlarge filter
             }
         }
     }
@@ -226,7 +225,7 @@ void enlarge(unsigned char img[][SIZE][RGB] ,int quarter_num)
         {
              for (int k = 0; k < RGB; k++)
              {
-                img[i][j][k] = img2[i][j][k];
+                img[i][j][k] = img2[i][j][k];   //copies which is done by the filter to image1
              }
         }
     }
@@ -247,7 +246,7 @@ void rotate_img(unsigned char img[][SIZE][RGB] ,string degree)
             {
                 for(int k = 0; k <  RGB ; k++)
                 {
-                    img2[i][j][k] = img[j][255 - i][k];
+                    img2[i][j][k] = img[j][255 - i][k];   //Equation to rotate the image by 90 deg.
                 }
             }
         }
@@ -261,7 +260,7 @@ void rotate_img(unsigned char img[][SIZE][RGB] ,string degree)
             {
                 for (int k = 0; k < RGB; k++)
                 {
-                    img2[i][j][k] = img[SIZE - i][SIZE - j][k];
+                    img2[i][j][k] = img[SIZE - i][SIZE - j][k];   //Equation to rotate the image by 180 deg.
                 }
             }
         }
@@ -274,7 +273,7 @@ void rotate_img(unsigned char img[][SIZE][RGB] ,string degree)
             for (int j = 0; j < SIZE; j++)
             {
                  for (int k = 0; k < RGB; k++){
-                    img2[i][j][k] = img[SIZE - j][i][k];
+                    img2[i][j][k] = img[SIZE - j][i][k];  //Equation to rotate the image by 270 deg.
                  }
             }
         }
@@ -283,7 +282,7 @@ void rotate_img(unsigned char img[][SIZE][RGB] ,string degree)
 
     for (int i = 0; i < SIZE; i++)
     {
-        for (int j = 0; j < SIZE; j++)
+        for (int j = 0; j < SIZE; j++)  //copies which is done by the filter to image1 
         {
              for (int k = 0; k < RGB; k++)
              {
@@ -300,7 +299,7 @@ void invert(unsigned char img[][SIZE][RGB])
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j< SIZE; j++) {
             for (int k = 0; k< RGB; k++) {
-                img[i][j][k] = 255 - img [i][j][k];
+                img[i][j][k] = 255 - img [i][j][k];   //Equation to perfrom Invert Filter
             }
         }
     }
